09c7a03781e3ed8d036733cd434e66ef9975bea6
Junior De La Roche <juniordelaroche@gmail.com>
2020-10-15 22:49:20 -0500

    docs agregado

---
+++ docs/precache-manifest.e6f678fa4be78f4de8dc7ee2699bd6e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a72ac8b3512029c3a2e81771c8c526",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d41e779fcbc0dbd14ab",
+    "url": "/static/css/main.55aa7060.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d41e779fcbc0dbd14ab",
+    "url": "/static/js/main.fb9f4cc4.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
